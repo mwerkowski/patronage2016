1593928add940ecffba9883e74b14a8e5b647c59
Marcin Werkowski <marcin.werkowski@blstream.com>
2015-12-17 17:47:12 +0100

    temp

--- src/app/index.route.js
+++ src/app/index.route.js
@@ -8,30 +8,49 @@
   /** @ngInject */
   function routerConfig($stateProvider, $urlRouterProvider) {
     $stateProvider
+      .state('index', {
+        abstract: true,
+        views: {
+          'menu': {
+            templateUrl: 'app/partials/menu.html'
+          },
+          'title': {
+            templateUrl: 'app/partials/title.html'
+          },
+          'content': {},
+          'foot': {
+            templateUrl: 'app/partials/foot.html'
+          }
+        }
+      })
       .state('home', {
+        parent: 'index',
         url: '/',
         templateUrl: 'app/main/home.html',
         controller: 'HomeController',
         controllerAs: 'home'
       })
       .state('users', {
+        parent: 'index',
         url: '/users?id',
         templateUrl: 'app/main/users.html',
         controller: 'UsersController',
         controllerAs: 'users'
       })
       .state('user', {
+        parent: 'index',
         url: '/users/:userName',
         templateUrl: 'app/main/user.html',
         controller: 'UserController',
         controllerAs: 'user'
       })
       .state('about', {
-          url: '/about',
-          templateUrl: 'app/main/about.html',
-          controller: 'AboutController',
-          controllerAs: 'about'
-        });
+        parent: 'index',
+        url: '/about',
+        templateUrl: 'app/main/about.html',
+        controller: 'AboutController',
+        controllerAs: 'about'
+      });
     $urlRouterProvider.otherwise('/');
   }
 
